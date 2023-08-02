b5d7529fcba580f56455179bac6a8565806c1a18
Joshua Entrata <jkentrata@gmail.com>
2023-08-02 15:09:21 +0800

    update cards styles and category data

--- src/components/blog/Card.jsx
+++ src/components/blog/Card.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import "./blog.css";
 import { blog } from "../../assets/data/data";
 import { Link } from "react-router-dom";
-import {
-  AiOutlineClockCircle,
-  AiOutlineShareAlt,
-  AiOutlineComment,
-  AiOutlineTags,
-} from "react-icons/ai";
+import { AiOutlineClockCircle } from "react-icons/ai";
 
 export const Card = () => {
   return (
@@ -20,21 +15,23 @@ export const Card = () => {
                 <img src={item.cover} alt="" />
               </div>
               <div className="details">
-                <div className="tag">
-                  <AiOutlineTags className="icon" />
-                  <a href="/">#{item.category}</a>
+                <div className="tags">
+                  {item.category.map((category, index) => (
+                    <div className="tag" key={index}>
+                      <a href="/">{category}</a>
+                    </div>
+                  ))}
                 </div>
+
                 <Link to={`/details/${item.id}`}>
                   <h3>{item.title}</h3>
                 </Link>
+
                 <p>{item.desc.slice(0, 180)}</p>
+
                 <div className="date">
                   <AiOutlineClockCircle className="icon" />
                   <label htmlFor="">{item.date}</label>
-                  <AiOutlineComment className="icon" />
-                  <label htmlFor="">27</label>
-                  <AiOutlineShareAlt className="icon" />
-                  <label htmlFor="">SHARE</label>
                 </div>
               </div>
             </div>
