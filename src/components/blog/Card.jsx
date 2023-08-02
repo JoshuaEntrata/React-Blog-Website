b53ce46d6c8819891de662e71c4792b2b15faf6e
Joshua Entrata <jkentrata@gmail.com>
2023-08-02 16:11:20 +0800

    link the whole card component

--- src/components/blog/Card.jsx
+++ src/components/blog/Card.jsx
@@ -10,31 +10,31 @@ export const Card = () => {
       <section className="blog">
         <div className="container grid">
           {blog.map((item) => (
-            <div className="box boxItems" key={item.id}>
-              <div className="img">
-                <img src={item.cover} alt="" />
-              </div>
-              <div className="details">
-                <div className="tags">
-                  {item.category.map((category, index) => (
-                    <div className="tag" key={index}>
-                      <a href="/">{category}</a>
-                    </div>
-                  ))}
+            <Link to={`/details/${item.id}`}>
+              <div className="box boxItems" key={item.id}>
+                <div className="img">
+                  <img src={item.cover} alt="" />
                 </div>
+                <div className="details">
+                  <div className="tags">
+                    {item.category.map((category, index) => (
+                      <div className="tag" key={index}>
+                        <a href="/">{category}</a>
+                      </div>
+                    ))}
+                  </div>
 
-                <Link to={`/details/${item.id}`}>
                   <h3>{item.title}</h3>
-                </Link>
 
-                <p>{item.desc.slice(0, 180)}</p>
+                  <p>{item.desc.slice(0, 180)}</p>
 
-                <div className="date">
-                  <AiOutlineClockCircle className="icon" />
-                  <label htmlFor="">{item.date}</label>
+                  <div className="date">
+                    <AiOutlineClockCircle className="icon" />
+                    <label htmlFor="">{item.date}</label>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </section>
