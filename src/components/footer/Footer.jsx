74bd56b22c6ea2d07bbf934dac756498d2f12158
Joshua Entrata <jkentrata@gmail.com>
2023-08-02 15:58:26 +0800

    modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import "./footer.css";
-import { AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
-import { RiInstagramFill } from "react-icons/ri";
+import { BsFacebook, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 export const Footer = () => {
   return (
     <>
       <footer className="box">
         <div className="container flex">
-          <p>LOrem lorem dfsaasdf erosd f cxlzc asdofsd</p>
+          <p>© Joshua Entrata</p>
           <div className="social">
-            <BsFacebook className="icon" />
-            <RiInstagramFill className="icon" />
-            <AiFillTwitterCircle className="icon" />
-            <AiFillLinkedin className="icon" />
+            <div className="socialbtns">
+              <a href="" className="socialbtn github">
+                <BsGithub />
+              </a>
+              <a href="" className="socialbtn linkedin">
+                <BsLinkedin />
+              </a>
+              <a href="" className="socialbtn facebook">
+                <BsFacebook />
+              </a>
+              <a href="" className="socialbtn instagram">
+                <BsInstagram />
+              </a>
+            </div>
           </div>
         </div>
       </footer>
