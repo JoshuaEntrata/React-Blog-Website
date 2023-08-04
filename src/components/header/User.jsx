c59499dccc50646ab11ac236eda9c06cd03a3fcc
Joshua Entrata <jkentrata@gmail.com>
2023-08-04 22:52:59 +0800

    delete account page and update user component

--- src/components/header/User.jsx
+++ src/components/header/User.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiChatUploadLine, RiContactsBookLine } from "react-icons/ri";
-import { IoHelp } from "react-icons/io5";
-import { BsEnvelopeHeart } from "react-icons/bs";
-import { BiHome, BiLogOutCircle } from "react-icons/bi";
+import { BiHome, BiUser } from "react-icons/bi";
 
 export const User = () => {
   const [profileOpen, setProfileOpen] = useState(false);
@@ -15,35 +13,49 @@ export const User = () => {
     <div>
       <div className="profile">
         <>
-          <button className="img" onClick={() => setProfileOpen(!profileOpen)}>
-            <img
-              src="https://i.pinimg.com/1200x/6a/7f/ba/6a7fba865bd67b02a800bc1d60b98686.jpg"
-              alt="images"
-            />
-          </button>
+          <div className="userBar">
+            <div className="text">
+              <h4>Joshua Entrata</h4>
+              <p>Mandaluyong, Philippines</p>
+            </div>
+            <button
+              className="img"
+              onClick={() => setProfileOpen(!profileOpen)}
+            >
+              <img
+                src="https://i.pinimg.com/1200x/6a/7f/ba/6a7fba865bd67b02a800bc1d60b98686.jpg"
+                alt="images"
+              />
+            </button>
+          </div>
 
           {profileOpen && (
             <div className="openProfile boxItems" onClick={close}>
-              <Link to="/account">
-                <div className="image">
-                  <div className="img">
-                    <img
-                      src="https://i.pinimg.com/1200x/6a/7f/ba/6a7fba865bd67b02a800bc1d60b98686.jpg"
-                      alt="images"
-                    />
-                  </div>
-
-                  <div className="text">
-                    <h4>Joshua Entrata</h4>
-                    <p>Mandaluyong, Philippines</p>
-                  </div>
+              <div className="image">
+                <div className="img">
+                  <img
+                    src="https://i.pinimg.com/1200x/6a/7f/ba/6a7fba865bd67b02a800bc1d60b98686.jpg"
+                    alt="images"
+                  />
                 </div>
-              </Link>
+
+                <div className="text">
+                  <h4>Joshua Entrata</h4>
+                  <p>Mandaluyong, Philippines</p>
+                </div>
+              </div>
 
               <Link to="/">
                 <button className="box">
                   <BiHome className="icon" />
                   <h4>Home</h4>
+                </button>
+              </Link>
+
+              <Link to="/about">
+                <button className="box">
+                  <BiUser class="icon" />
+                  <h4>About</h4>
                 </button>
               </Link>
 
